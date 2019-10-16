08fc33660fdf90a3bc0388363a15e2bf6b25093a
Dionisio Fernández <dionisio.fernandez.ortiz@gmail.com>
2019-10-16 17:01:47 +0200

    Start js
    Start of JavaScript

---
+++ js_ejercicios_clase/piramide/piramide.js
@@ -0,0 +1,13 @@
+function mostrarNumeros(){
+    function newFunction() {
+        for (var numero = 9; numero > 0; numero--) {
+            var listaNum = null;
+            for (var i = 0; i < 9; i++) {
+                listaNum += numero;
+                document.write(listaNum /n);
+
+            }
+            document.write(listaNum /n);
+        }
+    }
+}
